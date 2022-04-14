2069f854ce3faaad618eda0f8c2144ec5d377198
Deivid <deividdua@gmail.com>
2022-04-14 12:15:18 -0300

    Adjustment in README.md and resource eip.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "environment" {
-  description = "Will create eip if defined prod else will not create"
+  description = "Will create eip if defined prod in field default else will not create"
   type        = string
+  default     = "dev"
 }
 
 variable "eip_tag" {
