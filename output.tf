665ebbac8bfabde1d70a94493fbcec0e3325b9af
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2022-04-15 06:11:00 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-# output "public_ip" {
-#   description = "Contains the public IP address"
-#   value       = aws_eip.default.public_ip
-# }
+#aws_eip.web-eip[*].public_ipoutput "public_ip" {
+#  description = "Contains the public IP address"
+#  value       = aws_eip.web-eip[*].public_ip
+#}
